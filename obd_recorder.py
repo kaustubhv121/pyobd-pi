b69ace76e60b4b407d0c3f4e1fb1c7d8f26f4c8c
kaustubhv121 <kaustubhv121@gmail.com>
2015-07-06 10:03:27 -0500

    Update obd_recorder.py

--- obd_recorder.py
+++ obd_recorder.py
@@ -18,7 +18,7 @@ class OBD_Recorder():
         localtime = time.localtime(time.time())
         filename = path+"car-"+str(localtime[0])+"-"+str(localtime[1])+"-"+str(localtime[2])+"-"+str(localtime[3])+"-"+str(localtime[4])+"-"+str(localtime[5])+".log"
         self.log_file = open(filename, "w", 128)
-        self.log_file.write("Time,RPM,MPH,Throttle,Load,Fuel Status\n");
+        self.log_file.write("Time,RPM,MPH,MAF,Error Codes\n");
 
         for item in log_items:
             self.add_log_item(item)
@@ -94,7 +94,7 @@ class OBD_Recorder():
         return gear
         
 username = getpass.getuser()  
-logitems = ["rpm", "speed", "throttle_pos", "load", "fuel_status"]
+logitems = ["rpm", "speed", "maf", "dtc_status"]
 o = OBD_Recorder('/home/'+username+'/pyobd-pi/log/', logitems)
 o.connect()
 
